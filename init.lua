417c7d5034198e0b7d2dbbabf047b7fb61301da8
Olivier Bazin <obazin.dev@gmail.com>
2024-05-11 19:15:49 +0200

    feat: rename kickstart folder to 'me' for now

--- init.lua
+++ init.lua
@@ -301,7 +301,7 @@ require('lazy').setup({
     end,
   },
 
-  require 'kickstart.lang.lspconfig',
+  require 'me.lang.lspconfig',
 
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -509,7 +509,7 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  require 'kickstart.lang.treesitter',
+  require 'me.lang.treesitter',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -520,12 +520,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'me.plugins.debug',
+  require 'me.plugins.indent_line',
+  -- require 'me.plugins.lint',
+  require 'me.plugins.autopairs',
+  require 'me.plugins.neo-tree',
+  -- require 'me.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
