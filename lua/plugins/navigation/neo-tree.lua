ab9d438cdf2c7f74cff90eab43c69fb48f9a860e
Olivier Bazin <olivier.bazin@climateseed.com>
2024-08-21 15:45:09 +0200

    feat: show hidden files in Neotree

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -15,6 +15,18 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          -- '.git',
+          '.DS_Store',
+          'thumbs.db',
+        },
+        never_show = {},
+      },
       window = {
         mappings = {
           ['<leader>ee'] = 'close_window',
