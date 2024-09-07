3dd2f8021ead8fcb3d894963a61315343275a0a3
Olivier Bazin <olivierbazin.it@gmail.com>
2024-09-07 15:07:10 +0200

    feat: tweak ZenMode according to taste

--- lua/plugins/ui-modules/zen.lua
+++ lua/plugins/ui-modules/zen.lua
@@ -7,7 +7,7 @@ return {
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 120, -- width of the Zen window
+      width = 100, -- width of the Zen window
       height = 1, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
@@ -54,9 +54,9 @@ return {
       -- this will change the font size on wezterm when in zen mode
       -- See alse also the Plugins/Wezterm section in this projects README
       wezterm = {
-        enabled = false,
+        enabled = true,
         -- can be either an absolute font size or the number of incremental steps
-        font = '+4', -- (10% increase per step)
+        font = '+20', -- (10% increase per step)
       },
     },
     -- callback where you can add custom code when the Zen window opens
