75059fa589f728caaa9450e75891cdb216a9529e
Olivier Bazin <olivierbazin.it@gmail.com>
2024-09-07 22:05:37 +0200

    feat: add obsidian.nvim plugin

--- init.lua
+++ init.lua
@@ -43,7 +43,8 @@ require('lazy').setup {
   -- Tressitter support
   require 'plugins.lang.treesitter',
   -- require 'plugins.lang.debug',
-  require 'plugins.lang.markdown',
+  -- require 'plugins.lang.markdown',
+  require 'plugins.ui-modules.obsidian',
   require 'plugins.display.indent_line',
   require 'plugins.ui-modules.trouble',
   -- require 'plugins.lang.lint',
