742fac661e913b5c64199093b29212a8f7172723
Olivier Bazin <olivierbazin.it@gmail.com>
2024-10-02 19:57:30 +0200

    feat: basic tailwind support

--- lua/plugins/lang/lspconfig.lua
+++ lua/plugins/lang/lspconfig.lua
@@ -213,6 +213,14 @@ return { -- LSP Configuration & Plugins
         },
       },
       vtsls = {},
+      tailwindcss = {
+        -- exclude a filetype from the default_config
+        filetypes_exclude = { 'markdown' },
+        -- add additional filetypes to the default_config
+        filetypes_include = {},
+        -- to fully override the default_config, change the below
+        -- filetypes = {}
+      },
     }
 
     -- Ensure the servers and tools above are installed
