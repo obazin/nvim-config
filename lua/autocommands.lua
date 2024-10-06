456a35dbb566aef7356b66dd9fff6733f073574c
Olivier Bazin <olivierbazin.it@gmail.com>
2024-10-06 21:25:08 +0200

    feat: default folder opening is neotree in floating mode again

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -12,13 +12,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Auto-open Neo-Tree when entering a directory buffer
--- vim.api.nvim_create_autocmd('BufEnter', {
---   desc = 'Open Neo-Tree on startup if directory',
---   group = vim.api.nvim_create_augroup('neotree_start', { clear = true }),
---   callback = function()
---     local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
---     if stats and stats.type == 'directory' then
---       require('neo-tree.setup.netrw').hijack()
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Open Neo-Tree on startup if directory',
+  group = vim.api.nvim_create_augroup('neotree_start', { clear = true }),
+  callback = function()
+    local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
+    if stats and stats.type == 'directory' then
+      require('neo-tree.setup.netrw').hijack()
+    end
+  end,
+})
