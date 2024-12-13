8ec6d9a2d18e8ca082751c090bbcf2980ccff3e1
Olivier Bazin <olivierbazin.it@gmail.com>
2024-12-13 14:01:43 +0100

    feat: setup obsidian nvim to work with personnal vault (files & config)

--- lua/plugins/ui-modules/obsidian.lua
+++ lua/plugins/ui-modules/obsidian.lua
@@ -23,11 +23,52 @@ return {
         name = 'personal',
         path = '~/notes/personal-vault',
       },
-      {
-        name = 'work',
-        path = '~/notes/work',
-      },
     },
+    templates = {
+      folder = 'Core/Templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+    attachments = {
+      -- The default folder to place images in via `:ObsidianPasteImg`.
+      -- If this is a relative path it will be interpreted as relative to the vault root.
+      -- You can always override this per image by passing a full path to the command instead of just a filename.
+      img_folder = 'Core/Attachments', -- This is the default
+
+      -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+      ---@return string
+      img_name_func = function()
+        -- Prefix image names with timestamp.
+        return string.format('%s-', os.time())
+      end,
+
+      -- A function that determines the text to insert in the note when pasting an image.
+      -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+      -- This is the default implementation.
+      ---@param client obsidian.Client
+      ---@param path obsidian.Path the absolute path to the image file
+      ---@return string
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format('![%s](%s)', path.name, path)
+      end,
+    },
+    -- Optional, sort search results by "path", "modified", "accessed", or "created".
+    -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+    -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+    sort_by = 'modified',
+    sort_reversed = true,
+
+    -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+    search_max_lines = 1000,
+
+    -- Optional, determines how certain commands open notes. The valid options are:
+    -- 1. "current" (the default) - to always open in the current window
+    -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+    -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+    open_notes_in = 'vsplit',
 
     -- see below for full list of options 👇
   },
