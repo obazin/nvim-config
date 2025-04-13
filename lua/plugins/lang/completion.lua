c4e582d3569a3ff33dbfaa6693183e975b8fa378
Olivier Bazin <olivierbazin.it@gmail.com>
2025-04-13 20:22:25 +0200

    feat: fine tune blink for better completion search

--- lua/plugins/lang/completion.lua
+++ lua/plugins/lang/completion.lua
@@ -56,7 +56,7 @@ return { -- Autocompletion
       -- <c-k>: Toggle signature help
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
+      preset = 'enter',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -90,7 +90,7 @@ return { -- Autocompletion
     -- the rust implementation via `'prefer_rust_with_warning'`
     --
     -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'lua' },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
