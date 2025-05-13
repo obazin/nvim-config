4325efb3694ce3c8d96f4254bee534649c27b424
Olivier Bazin <olivierbazin.it@gmail.com>
2025-05-13 18:21:13 +0200

    fix: adapt rustacean config to mason-registry recent changes

--- lua/plugins/lang/rustacean.lua
+++ lua/plugins/lang/rustacean.lua
@@ -1,13 +1,16 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  version = '^6',
+  lazy = false,
   config = function()
-    local mason_registry = require 'mason-registry'
-    local codelldb = mason_registry.get_package 'codelldb'
-    local extension_path = codelldb:get_install_path() .. '/extension/'
-    local codelldb_path = extension_path .. 'adapter/codelldb'
-    local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+    -- Get Mason root
+    local mason_path = vim.fn.expand '$MASON'
+    local package_path = mason_path .. '/packages/codelldb/extension/'
+    local codelldb_path = package_path .. 'adapter/codelldb'
+
+    -- You'll need to adjust this for your OS if not macOS
+    local liblldb_path = package_path .. 'lldb/lib/liblldb.dylib'
+
     local cfg = require 'rustaceanvim.config'
 
     vim.g.rustaceanvim = {
