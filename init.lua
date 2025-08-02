fa3522ceefe64c917e7e7b952ddc8f78640dd147
Olivier Bazin <olivierbazin.it@gmail.com>
2025-08-02 12:01:43 +0200

    feat: Add useful vim options, and put every options in options.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-vim.o.laststatus = 3
-
-vim.opt.formatoptions = 'tcqn'
-vim.opt.wrap = true
-vim.opt.textwidth = 120
-vim.o.conceallevel = 2
-
 require 'options'
 require 'keymaps'
 require 'autocommands'
--- lua/options.lua
+++ lua/options.lua
@@ -1,77 +1,44 @@
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+local vim = vim
 
--- Make line numbers default
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
 
--- Identation settings : tab = 4 spaces, rust reco
+vim.opt.breakindent = true
+vim.opt.formatoptions = 'tcqn'
+vim.opt.wrap = true
+vim.opt.textwidth = 120
+vim.o.conceallevel = 2
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-vim.opt.swapfile = false
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.fileencoding = 'utf-8'
+vim.opt.writebackup = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.swapfile = false
 
--- Decrease update time
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
 vim.opt.timeoutlen = 500
 
--- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- Global status on
-vim.o.laststatus = 3
-
--- Fine tune diagnostics look & feel (virutal text can sometimes be invasive)
-vim.diagnostic.config {
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
-}
+vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.laststatus = 3 -- Global status on
+vim.opt.hlsearch = true
+vim.opt.inccommand = 'split'
+vim.opt.showmode = false
